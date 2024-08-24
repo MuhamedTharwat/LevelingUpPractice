acabdc3efa237d98117967c233089cbdc8226ff6
MuahmedTharwat <yourGitHub@email.com>
2024-08-24 09:13:38 +0300

    add logging to ActionBot.java

--- src/main/java/utils/actions/ActionBot.java
+++ src/main/java/utils/actions/ActionBot.java
@@ -1,5 +1,7 @@
 package utils.actions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -10,8 +12,9 @@ import org.openqa.selenium.support.ui.Wait;
 import java.time.Duration;
 
 public class ActionBot {
-    WebDriver driver;
-    Wait<WebDriver> wait;
+    private WebDriver driver;
+    private Wait<WebDriver> wait;
+    private final static Logger log= LogManager.getLogger(ActionBot.class);
 
     public ActionBot(WebDriver driver) {
         this.driver = driver;
@@ -34,6 +37,7 @@ public class ActionBot {
         driver.manage().window().setPosition(new Point(0, 0));
         driver.manage().window().setSize(new Dimension(1920, 1080));
         this.wait = createFluentWait(this.driver, 10, 800);
+        log.info("Initialized {} browser.", browser);
     }
 
     public String getPageTitle() {
@@ -42,56 +46,79 @@ public class ActionBot {
 
     public void navigate(String url) {
         this.driver.navigate().to(url);
+        log.info("Navigated to URL: {}", url);
     }
 
     public void type(By locator, String text) {
-        wait.until(driver1 -> {
+        try {
+            waitForElement(locator);
             driver.findElement(locator).sendKeys(text);
-            return true;
-        });
+            log.info("Text Typed '{}' into element: {}", text, locator);
+        } catch (Exception e) {
+            log.error("Unable to type form element: {}. with Exception: {}", locator, e.getMessage());
+        }
     }
 
     public void submit(By locator) {
-        wait.until(driver1 -> {
+        try {
+            waitForElement(locator);
             driver.findElement(locator).submit();
-            return true;
-        });
+            log.info("Submitted form element: {}", locator);
+        } catch (Exception e) {
+            log.error("Unable to submit form element: {}. with Exception: {}", locator, e.getMessage());
+        }
     }
 
     public void click(By locator) {
-        wait.until(driver1 -> {
+        try {
+            waitForElement(locator);
             driver.findElement(locator).click();
-            return true;
-        });
+            log.info("Clicked element: {}", locator);
+        } catch (Exception e) {
+            log.error("Unable to click element: {}. with Exception: {}", locator, e.getMessage());
+        }
     }
 
     public String getText(By locator) {
-        final String[] elementText = new String[1];
-        wait.until(driver -> {
-
-            elementText[0] = driver.findElement(locator).getText();
-            return true;
-        });
-        return elementText[0];
+        try {
+            waitForElement(locator);
+            String text= this.driver.findElement(locator).getText();
+            log.info("Retrieved text '{}' from element: {}", text, locator);
+            return text;
+        } catch (Exception e) {
+            log.error("Unable to retrieve text from element: {}. with Exception: {}", locator, e.getMessage());
+            return "";
+        }
     }
 
     public boolean elementDisplayed(By locator) {
-        wait.until(driver1 -> {
-            driver.findElement(locator);
-            return true;
-        });
-        return driver.findElement(locator).isDisplayed();
+        try {
+            waitForElement(locator);
+            Boolean displayed= driver.findElement(locator).isDisplayed();
+            log.info("Element displayed status for {}: {}", locator, displayed);
+            return displayed;
+        } catch (Exception e) {
+            log.error("Element not displayed: {}. Exception: {}", locator, e.getMessage());
+            return false;
+        }
     }
 
     public void closeDriver() {
         if (this.driver != null) {
             this.driver.quit();
             this.driver = null;
+            log.info("WebDriver closed successfully.");
         }
     }
 
     private Wait<WebDriver> createFluentWait(WebDriver driver, int timeoutSeconds, int pollingMillis) {
         return new FluentWait<>(driver).withTimeout(Duration.ofSeconds(timeoutSeconds)).pollingEvery(Duration.ofMillis(pollingMillis)).ignoring(NotFoundException.class).ignoring(ElementNotInteractableException.class).ignoring(StaleElementReferenceException.class).ignoring(AssertionError.class);
+    }
+    private void waitForElement(By locator){
+        this.wait.until(d -> {
+            this.driver.findElement(locator);
+            return true;
+        });
     }
 
 }
